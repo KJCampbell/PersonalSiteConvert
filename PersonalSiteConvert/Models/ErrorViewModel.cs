f10b1ae9ca256b135b264465868024a9afde0087
Muffin <kellan.campbell@outlook.com>
2023-04-03 15:25:24 -0500

    Add project files.

---
+++ PersonalSiteConvert/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PersonalSiteConvert.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
